e9356af01239aef0741de10159b66058fabbdcca
Ronae Johnson <ronae2004@gmail.com>
2022-04-11 20:44:00 -0500

    form.py updated

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import InputRequired
 from wtforms import validators
 from flask_wtf.file import FileField, FileRequired, FileAllowed
@@ -8,18 +8,16 @@ from wtforms.fields.html5 import EmailField
 class LoginForm(FlaskForm):
     #Feilds for login form
     username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.InputRequired()])
 
 class RegistrationForm(FlaskForm):
     #Feilds for registration form
     username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    password = PasswordField('Password', [validators.InputRequired()])
     fullname = StringField('Fullname', validators=[InputRequired()])
     email = EmailField('Email', [validators.InputRequired(), validators.Email()])
     location = StringField('Location', validators=[InputRequired()])
-    biography = TextAreaField('Biography', validators=[DataRequired()])
+    biography = TextAreaField('Biography', validators=[InputRequired()])
     photo = FileField('UploadPhoto', validators=[FileRequired(), FileAllowed(['jpg','png'])])
 
 
-
-    
