bf4389e9974308b35d3feec6c9902cb862f12b46
Ronae Johnson <ronae2004@gmail.com>
2022-04-11 21:21:27 -0500

    for checkout

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField
+from wtforms import StringField, PasswordField, TextAreaField, IntegerField
 from wtforms.validators import InputRequired
 from wtforms import validators
 from flask_wtf.file import FileField, FileRequired, FileAllowed
@@ -20,4 +20,18 @@ class RegistrationForm(FlaskForm):
     biography = TextAreaField('Biography', validators=[InputRequired()])
     photo = FileField('UploadPhoto', validators=[FileRequired(), FileAllowed(['jpg','png'])])
 
+class ExploreForm(FlaskForm):
+    make = StringField('Make', validators=[InputRequired()])
+    model = IntegerField('Model', validators=[DataRequired()])
 
+class AddNewCarForm(FlaskForm):
+    make = StringField('Make', validators=[InputRequired()])
+    model = StringField('Model', validators=[InputRequired()])
+    colour = StringField('Colour', validators=[InputRequired()])
+    year = IntegerField('Year', validators=[InputRequired()])
+    price = IntegerField('Price', validators=[InputRequired()])
+    cartype = StringField('Car Type', validators=[InputRequired()])
+    transmission = StringField('Transmission', validators=[InputRequired()])
+    description =  TextAreaField('Description', validators=[InputRequired()])
+    photo = FileField('UploadPhoto', validators=[FileRequired(), FileAllowed(['jpg','png'])])
+    
